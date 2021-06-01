ca7e0da62ab3edb35f99bd25855824f3ec6983d6
ARaufy <innocentrauf8@gmail.com>
2021-05-31 23:42:21 -0400

    End of section 4

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using API.Entities;
 using API.Interfaces;
@@ -16,7 +20,25 @@ namespace API.Services
 
         public string CreateToken(AppUser user)
         {
-            throw new System.NotImplementedException();
+            var claims = new List<Claim>
+            {
+              new Claim(JwtRegisteredClaimNames.NameId, user.UserName)  
+            };
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDiscriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDiscriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
