e0dc0a90f7765d55573d04f75825637bbf595de8
ARaufy <innocentrauf8@gmail.com>
2021-06-04 20:44:00 -0400

    55

--- client1/src/app/nav/nav.component.ts
+++ client1/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 import { AccountService } from '../_services/account.service';
 
 
@@ -9,23 +11,27 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn: boolean = false;
-  constructor(private accountSeervice: AccountService) { }
+ 
+
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
+    
   }
 
   login(){
-    this.accountSeervice.login(this.model).subscribe(response => {
+    this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     });
   }
 
   logout(){
-    this.loggedIn = false;
+    this.accountService.logout();
+    
   }
+
+
 
 }
