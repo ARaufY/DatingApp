6569b0509fb6aaeb76833cddffd87cfbee01f095
ARaufy <innocentrauf8@gmail.com>
2021-06-25 03:19:20 -0400

    end of section 12

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -6,5 +6,11 @@ namespace API.DTOs
 
         public string Token { get; set; }
         public string PhotoUrl { get; set; }
+
+        public string KnownAs { get; set; }
+        
+        
+
+
     }
 }
